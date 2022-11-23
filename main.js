edad4266c4aaaea2771d9a8f2aa07729f1e39d1a
Kerim-tk <alekperovkerim@gmail.com>
2022-11-23 10:56:12 +0400

    function

---
+++ main.js
@@ -0,0 +1,144 @@
+var elephant = {
+    aname: 'tosha',
+    age: 2,
+    food: 10,
+    typeofAnimal: "elephant",
+    biome: "tropic",
+    isNeedfulReservoir: true,
+    requiredSpace: 50,
+    diet: "greenFood"
+};
+var elephant2 = {
+    aname: 'gosha',
+    age: 5,
+    food: 15,
+    typeofAnimal: "elephant",
+    biome: "tropic",
+    isNeedfulReservoir: true,
+    requiredSpace: 50,
+    diet: "greenFood"
+};
+var lion = {
+    aname: "pumba",
+    age: 5,
+    food: 30,
+    typeofAnimal: "cat",
+    biome: "savanna",
+    isNeedfulReservoir: true,
+    requiredSpace: 100,
+    diet: "meet"
+};
+var tiger = {
+    aname: "timon",
+    age: 6,
+    food: 35,
+    typeofAnimal: "cat",
+    biome: "savanna",
+    isNeedfulReservoir: true,
+    requiredSpace: 100,
+    diet: "meet"
+};
+var turtle = {
+    aname: "Rafael",
+    age: 25,
+    food: 1,
+    typeofAnimal: "turtle",
+    biome: "canalization",
+    isNeedfulReservoir: false,
+    requiredSpace: 25,
+    diet: "greenFood"
+};
+var Rafael = {
+    aname: "ninja",
+    age: 20,
+    food: 9999,
+    typeofAnimal: "turtle",
+    biome: "canalization",
+    isNeedfulReservoir: false,
+    requiredSpace: 25,
+    diet: "greenFood"
+};
+var redFish = {
+    aname: "Nemo",
+    age: 1,
+    food: 1,
+    typeofAnimal: "fish",
+    biome: "aquatic",
+    isNeedfulReservoir: false,
+    requiredSpace: 1,
+    diet: "seeds"
+};
+var canalization = {
+    biome: "canalization",
+    reservoir: false,
+    area: 50,
+    animals: [],
+    name: "canalization1"
+};
+var aquarium = {
+    biome: "aquatic",
+    reservoir: false,
+    area: 5,
+    animals: [],
+    name: "aquarium1"
+};
+var cage1 = {
+    biome: "savanna",
+    reservoir: true,
+    area: 200,
+    animals: [],
+    name: "cage1"
+};
+var cage2 = {
+    biome: "tropic",
+    reservoir: true,
+    area: 150,
+    animals: [],
+    name: "cage2"
+};
+function placeAnimals(animal, enclosure) {
+    if (animal.isNeedfulReservoir === enclosure.reservoir && animal.biome === enclosure.biome && animal.requiredSpace <= enclosure.area) {
+        console.log("This enclosure is suitable for ".concat(animal.aname, "."));
+        checkType(animal, enclosure.animals);
+        return "".concat(animal.aname, " added to ").concat(enclosure.name);
+    }
+    else {
+        console.log("It is impossible to add \"".concat(animal.aname, "\" to enclosure with ").concat(enclosure.area, "m area,").concat(enclosure.biome, " biome. "));
+        return 'That is why adding failed.';
+    }
+}
+function checkType(animal, animals) {
+    for (var i = 0; i < animals.length; i++) {
+        if ((animals[i].typeofAnimal == 'lion' && animal.typeofAnimal == 'herbivore') || (animals[i].typeofAnimal == 'herbivore' && animal.typeofAnimal == 'lion')) {
+            console.log("This enclosure is not suitable for ".concat(animal.aname, ",because types of animals are opposite. "));
+            console.log('====================================');
+        }
+        else {
+            animals.push(animal);
+        }
+    }
+    //    return animal && animals
+}
+console.log(placeAnimals(lion, cage1));
+console.log(placeAnimals(redFish, aquarium));
+console.log(placeAnimals(Rafael, canalization));
+console.log(placeAnimals(turtle, canalization));
+console.log(placeAnimals(elephant, cage2));
+console.log(placeAnimals(elephant2, cage2));
+console.log(placeAnimals(lion, aquarium));
+console.log('====================================');
+function showAnimals(enclosure) {
+    console.log("Animals in ".concat(enclosure.name, ":"));
+    enclosure.animals.forEach(function (anm) {
+        console.log(anm.aname);
+        if (enclosure.animals.length === 0) {
+            console.log("This enclosure is empty..");
+        }
+        return anm.aname;
+    });
+}
+showAnimals(cage1);
+showAnimals(cage2);
+showAnimals(canalization);
+showAnimals(aquarium);
+console.log(showAnimals(cage1));
--- main.ts
+++ main.ts
@@ -1,127 +1,178 @@
+
 interface IAnimalTypes {
-    animal: string;
+    typeofAnimal: string;
     biome: string;
-    reservoir: boolean;
+    isNeedfulReservoir: boolean;
     requiredSpace: number;
     diet: string;
-    predator: boolean;
-    herbivore: boolean
 }
+type Type= 'predator' | 'herbivore';
 interface IAnimal extends IAnimalTypes {
-    name: string;
+    aname: string;
     age: number;
     food: number;
 }
 const elephant: IAnimal = {
-    name: 'tosha',
+    aname: 'tosha',
     age: 2,
     food: 10,
-    animal: "elephant",
+    typeofAnimal: "elephant",
     biome: "tropic",
-    reservoir: true,
+    isNeedfulReservoir: true,
     requiredSpace: 50,
-    diet: "greenFood",
-    predator: true,
-    herbivore: false
+    diet: "greenFood"
 }
 const elephant2: IAnimal = {
-    name: 'gosha',
+    aname: 'gosha',
     age: 5,
     food: 15,
-    animal: "elephant",
+    typeofAnimal: "elephant",
     biome: "tropic",
-    reservoir: true,
+    isNeedfulReservoir: true,
     requiredSpace: 50,
-    diet: "greenFood",
-    predator: true,
-    herbivore: false
+    diet: "greenFood"
 }
 const lion: IAnimal = {
-    name: "pumba",
+    aname: "pumba",
     age: 5,
     food: 30,
-    animal: "cat",
+    typeofAnimal: "cat",
     biome: "savanna",
-    reservoir: true,
+    isNeedfulReservoir: true,
     requiredSpace: 100,
-    diet: "meet",
-    predator: true,
-    herbivore: false
+    diet: "meet"
 }
 const tiger: IAnimal = {
-    name: "timon",
+    aname: "timon",
     age: 6,
     food: 35,
-    animal: "cat",
+    typeofAnimal: "cat",
     biome: "savanna",
-    reservoir: true,
+    isNeedfulReservoir: true,
     requiredSpace: 100,
-    diet: "meet",
-    predator: true,
-    herbivore: false
+    diet: "meet"
 }
 const turtle: IAnimal = {
-    name: "Rafael",
+    aname: "Rafael",
     age: 25,
     food: 1,
-    animal: "turtle",
+    typeofAnimal: "turtle",
     biome: "canalization",
-    reservoir: false,
+    isNeedfulReservoir: false,
     requiredSpace: 25,
-    diet: "greenFood",
-    predator: false,
-    herbivore: true
+    diet: "greenFood"
 }
 const Rafael: IAnimal = {
-    name: "ninja",
+    aname: "ninja",
     age: 20,
     food: 9999,
-    animal: "turtle",
+    typeofAnimal: "turtle",
     biome: "canalization",
-    reservoir: false,
+    isNeedfulReservoir: false,
     requiredSpace: 25,
-    diet: "greenFood",
-    predator: false,
-    herbivore: true
+    diet: "greenFood"
 }
 const redFish: IAnimal = {
-    name: "Nemo",
+    aname: "Nemo",
     age: 1,
     food: 1,
-    animal: "fish",
+    typeofAnimal: "fish",
     biome: "aquatic",
-    reservoir: false,
+    isNeedfulReservoir: false,
     requiredSpace: 1,
-    diet: "seeds",
-    predator: false,
-    herbivore: true
+    diet: "seeds"
 }
 interface IAviaries {
+    name:string,
     biome: string;
     reservoir: boolean;
     area: number;
+    animals:IAnimal[]
 }
 const canalization: IAviaries = {
     biome: "canalization",
     reservoir: false,
-    area: 50
+    area: 50,
+    animals: [],
+    name: "canalization1"
 }
 const aquarium: IAviaries = {
     biome: "aquatic",
     reservoir: false,
-    area: 5
+    area: 5,
+    animals: [],
+    name: "aquarium1"
 }
 const cage1: IAviaries = {
     biome: "savanna",
     reservoir: true,
-    area: 200
+    area: 200,
+    animals: [],
+    name: "cage1"
 }
 const cage2: IAviaries = {
     biome: "tropic",
     reservoir: true,
-    area: 150
+    area: 150,
+    animals: [],
+    name: "cage2"
 }
 
+function placeAnimals(animal:IAnimal,enclosure:IAviaries) {
+    if (animal.isNeedfulReservoir===enclosure.reservoir && animal.biome===enclosure.biome && animal.requiredSpace<=enclosure.area) {
+                 
+    console.log(`This enclosure is suitable for ${animal.aname}.`); 
+    checkType(animal,enclosure.animals);
+   
+    return  `${animal.aname} added to ${enclosure.name}`;
+        
+    }
+    else{
+        console.log(`It is impossible to add "${animal.aname}" to enclosure with ${enclosure.area}m area,${enclosure.biome} biome. `);
+        return 'That is why adding failed.'
+    }
+
+}
+function checkType(animal:IAnimal,animals:IAnimal[]) {
+for (let i = 0; i < animals.length; i++) {
+if ((animals[i].typeofAnimal=='lion' && animal.typeofAnimal=='herbivore') || (animals[i].typeofAnimal=='herbivore' && animal.typeofAnimal=='lion')) {
+    console.log(`This enclosure is not suitable for ${animal.aname},because types of animals are opposite. `);
+    console.log('====================================');
+}
+else{
+    animals.push(animal);
+    
+}
+
+}
+//    return animal && animals
+}
+
+console.log(placeAnimals(lion,cage1));
+console.log(placeAnimals(redFish, aquarium));
+console.log(placeAnimals(Rafael, canalization));
+console.log(placeAnimals(turtle, canalization));
+console.log(placeAnimals(elephant, cage2));
+console.log(placeAnimals(elephant2, cage2));
+console.log(placeAnimals(lion,aquarium));
+
+console.log('====================================');
+
+function showAnimals(enclosure:IAviaries) {
+console.log(`Animals in ${enclosure.name}:`);
+enclosure.animals.forEach(anm => {
+console.log(anm.aname)
+if (enclosure.animals.length===0) {
+    console.log("This enclosure is empty..");
+    
+}
+return anm.aname
+})
+}
+showAnimals(cage1);
+showAnimals(cage2);
+showAnimals(canalization);
+showAnimals(aquarium);
 
 
-
+console.log(cage2.animals);
